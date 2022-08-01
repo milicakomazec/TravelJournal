949708fadcdf5bbea3b50a4bc5959828f87f332d
milicakomazec <milicakomazec.it@gmail.com>
2022-08-01 20:00:07 +0200

    add data.js file with data

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -1,8 +1,10 @@
 import "./Page.css";
 import React from "react";
 import Header from "../Header";
+import data from "../../data.js";
 
 const Page = () => {
+  //   console.log(data);
   return (
     <div className="page">
       <Header />
---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    location: "Japan",
+    title: "Mount Fuji",
+    dateFrom: new Date("01/12/2021"),
+    dateTo: new Date("01/24/2021"),
+    text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  },
+  {
+    id: 2,
+    location: "Australia",
+    title: "Sydney Opera House",
+    dateFrom: new Date("05/27/2021"),
+    dateTo: new Date("06/08/2021"),
+    text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+  },
+  {
+    id: 3,
+    location: "Norway",
+    title: "Geirangerfjord",
+    dateFrom: new Date("10/01/2021"),
+    dateTo: new Date("11/18/2021"),
+    text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+  },
+];
+
+export default data;
