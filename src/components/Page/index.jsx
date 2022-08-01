849da3688088fd1b110900fd90e47ef76ff9585d
milicakomazec <milicakomazec.it@gmail.com>
2022-08-01 19:25:59 +0200

    add Page component

---
+++ src/components/Page/index.jsx
@@ -0,0 +1,13 @@
+import "./Page.css";
+import React from "react";
+import Header from "../Header";
+
+const Page = () => {
+  return (
+    <div className="page">
+      <Header />
+    </div>
+  );
+};
+
+export default Page;
