22f8f300c5276e244ac268d494d0fac987f1ef18
milicakomazec <milicakomazec.it@gmail.com>
2022-08-03 22:42:10 +0200

    make responsive for smaller screens

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import "./Card.css";
 import moment from "moment";
 import PropTypes from "prop-types";
+import { useMediaQuery } from "react-responsive";
 
 const Card = ({ title, location, dateFrom, dateTo, text, img, link }) => {
-  console.log(dateFrom, dateTo);
+  // console.log(dateFrom, dateTo);
+  const isMobile = useMediaQuery({
+    //true kad je manji 480px
+    query: "(max-width: 480px)",
+  });
   return (
     <div className="card">
       <div className="card--wrapper">
@@ -12,26 +17,27 @@ const Card = ({ title, location, dateFrom, dateTo, text, img, link }) => {
           src={img}
           alt=""
           className="location-img"
-          height={200}
-          width={150}
+          height={270}
+          width={230}
         ></img>
         <div className="card--right-side">
-          <div className="location--container">
-            <div className="location--wrapper">
-              <img
-                src="../../media/location.png"
-                alt="location icon"
-                className="location--icon"
-              ></img>
-              <div className="location">{location.toUpperCase()}</div>
+          {!isMobile && (
+            <div className="location--container">
+              <div className="location--wrapper">
+                <img
+                  src="../../media/location.png"
+                  alt="location icon"
+                  className="location--icon"
+                ></img>
+                <div className="location">{location.toUpperCase()}</div>
+              </div>
+              <div className="location--maps-link">
+                <a href={link} style={{ textDecoration: "none" }}>
+                  View on Google Maps
+                </a>
+              </div>
             </div>
-
-            <div className="location--maps-link">
-              <a href={link} style={{ textDecoration: "none" }}>
-                View on Google Maps
-              </a>
-            </div>
-          </div>
+          )}
           <div className="card--title">{title}</div>
           <div className="date-wrapper">
             <div style={{ fontWeight: "600" }}>
@@ -42,6 +48,23 @@ const Card = ({ title, location, dateFrom, dateTo, text, img, link }) => {
           <div className="text">{text}</div>
         </div>
       </div>
+      {isMobile && (
+        <div className="location--container">
+          <div className="location--wrapper">
+            <img
+              src="../../media/location.png"
+              alt="location icon"
+              className="location--icon"
+            ></img>
+            <div className="location">{location.toUpperCase()}</div>
+          </div>
+          <div className="location--maps-link">
+            <a href={link} style={{ textDecoration: "none" }}>
+              View on Google Maps
+            </a>
+          </div>
+        </div>
+      )}
       <div className="horizontal-line"></div>
     </div>
   );
